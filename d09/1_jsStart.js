78968fc99e091d0e89df45ca169e8934af54aba0
IceP0tato <rlawodud0313@gmail.com>
2025-06-09 12:16:36 +0900

    Update d09

---
+++ d09/1_jsStart.js
@@ -0,0 +1,7 @@
+// 주석 (한 줄)
+/*  
+    주석
+    (여러 줄)
+*/
+
+console.log("Hello World 2")
---
+++ d09/2_output.js
@@ -0,0 +1,20 @@
+10 // Number
+3.14 // Number
+"Hello" // String
+true // Boolean
+[10, 3.14, "Hello", true] // Array
+
+// 1. 콘솔 (개발자 도구)에 출력
+console.log(10);
+console.log("안녕하세요");
+
+// 2. 브라우저 알림 창으로 출력
+alert(3.14);
+alert(true);
+
+// 3. HTML 문서에 출력
+document.write("<h3>Hello World</h3>");
+
+// 4. 특정한 선택자 (markup, class, id)에 출력
+// innerHTML = <마크업>과 </마크업> 사이
+document.querySelector("body").innerHTML = "<h3>JS TEST</h3>"
