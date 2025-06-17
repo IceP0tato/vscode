01b4fa92dae810b884a8c04c69ff552232618076
IceP0tato <rlawodud0313@gmail.com>
2025-06-17 18:19:17 +0900

    Update d16

--- d15/실습/실습10.js
+++ d15/실습/실습10.js
@@ -1,9 +1,54 @@
+document.write(`<h1>학생</h1>`);
+
+
+
 const studentInfo = [
-    { sId: 1, name: "홍길동", major: "조리학과" }
-]; // 학생정보: 학번, 이름, 전공
+    { name: '재영', sid: '1', major: '컴퓨터공학과' },
+    { name: '철수', sid: '2', major: '인공지능학과' },
+    { name: '영희', sid: '3', major: '데이터사이언스학과' }
+]
+
 const subjectInfo = [
-    { subId: 1, subName: "국어", profName: "김철수" }
-]; // 과목정보: 과목코드, 과목명, 교수자명
+    { subid: '11', subname: '프로그래밍', profname: '이교수' },
+    { subid: '11', subname: '프로그래밍', profname: '최교수' },
+    { subid: '11', subname: '프로그래밍', profname: '이교수' }
+]
+
+
 const regInfo = [
-    { regId: 1, sid: 1, subId: 1, score: 'B' }
-]; // 수강신청내역정보: 수강신청내역코드, 학번, 과목코드, 성적
+    { regid: '20', sid: '1', subid: '11', grad: 'A' },
+    { regid: '20', sid: '1', subid: '11', grad: 'B' },
+    { regid: '20', sid: '1', subid: '11', grad: 'C' }
+]
+let pass = false;
+let names = "";
+let studen = "";
+let subject = "";
+let 이수여부 = '';
+for (let i = 0; i <= studentInfo.length - 1; i++) {
+    let studen = studentInfo[i];
+    for (let a = 0; a <= subjectInfo.length - 1; a++) {
+        subject = subjectInfo[a];
+
+    }
+}
+
+let html = "";
+for (let c = 0; c <= studentInfo.length - 1; c++) {
+    let reg = regInfo[c];
+    if (regInfo[c].grad == 'F') {
+        이수여부 = "fail";
+    } else {
+        이수여부 = "pass";
+    }
+    html += `<h4> ${studentInfo[c].name} (${studentInfo[c].major})</h4>`
+    html += `<hr/>
+            <div>
+            <h4>  웹 프로그래밍 기초 <span>(${subjectInfo[c].profname})</span></h4>
+                <ul>
+                    <li>성적 : ${regInfo[c].grad} </li>
+                    <li>이수 여부 : ${이수여부} </li>
+                </ul> 
+                </div>`
+}
+document.write(html);
